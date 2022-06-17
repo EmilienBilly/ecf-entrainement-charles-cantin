7b9842542224ba2787335abb90a9cfce2a0c5601
Emilien Billy <emilien@macbook-pro-de-emilien.home>
2022-06-17 23:25:41 +0200

    moved Navbar styling from global.css to Navbar.module.css

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,29 +1,32 @@
+import { useState } from "react";
+import styles from "../styles/Navbar.module.css";
+
 const Navbar = () => {
     return (
-        <nav className="navbar">
+        <nav className={styles.navbar}>
             <div>
                 <a>
-                    <img src="/facebook-icon.svg" className="social-media-icon" alt="" />
+                    <img src="/facebook-icon.svg" className={styles.socialMediaIcon} alt="" />
                 </a>
                 <a>
-                    <img src="/instagram-icon.svg" className="social-media-icon" alt="" />
+                    <img src="/instagram-icon.svg" className={styles.socialMediaIcon} alt="" />
                 </a>
             </div>
-            <ul className="nav-menu">
-                <li className="nav-item">
-                    <a className="nav-link">GALERIE</a>
+            <ul className={styles.navMenu}>
+                <li className={styles.navItem}>
+                    <a className={styles.navLink}>GALERIE</a>
                 </li>
-                <li className="nav-item">
-                    <a className="nav-link">TARIFS & PRÉSATIONS</a>
+                <li className={styles.navItem}>
+                    <a className={styles.navLink}>TARIFS & PRÉSATIONS</a>
                 </li>
-                <li className="nav-item">
-                    <a className="nav-link">CONTACT</a>
+                <li className={styles.navItem}>
+                    <a className={styles.navLink}>CONTACT</a>
                 </li>
             </ul>
-            <div className="hamburger">
-                <span className="bar" />
-                <span className="bar" />
-                <span className="bar" />
+            <div className={styles.hamburger}>
+                <span className={styles.bar} />
+                <span className={styles.bar} />
+                <span className={styles.bar} />
             </div>
         </nav>
     );
