591ca5464f22457f870735b95f7b91db2b04b280
EmilienBilly <emilien.billy@gmail.com>
2022-06-18 19:19:55 +0200

    hamburger onclick toggling navmenu

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,9 +1,19 @@
 import { useState } from "react";
 import styles from "../styles/Navbar.module.css";
+import Link from "next/link";
 
 const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const handleClick = () => {
+        setIsOpen(!isOpen);
+    };
+
+    const hamburgerClassName = isOpen ? `${styles.hamburger} ${styles.active}` : styles.hamburger;
+    const navMenuClassName = isOpen ? `${styles.navMenu} ${styles.active}` : styles.navMenu;
+    const navbarClassName = isOpen ? `${styles.navbar} ${styles.active}` : styles.navbar;
+
     return (
-        <nav className={styles.navbar}>
+        <nav className={navbarClassName}>
             <div>
                 <a>
                     <img src="/facebook-icon.svg" className={styles.socialMediaIcon} alt="" />
@@ -12,18 +22,33 @@ const Navbar = () => {
                     <img src="/instagram-icon.svg" className={styles.socialMediaIcon} alt="" />
                 </a>
             </div>
-            <ul className={styles.navMenu}>
+            <ul className={navMenuClassName}>
                 <li className={styles.navItem}>
-                    <a className={styles.navLink}>GALERIE</a>
+                    <Link href="/gallery">
+                        <a className={styles.navLink} onClick={handleClick}>
+                            GALERIE
+                        </a>
+                    </Link>
                 </li>
                 <li className={styles.navItem}>
-                    <a className={styles.navLink}>TARIFS & PRÉSATIONS</a>
+                    <a className={styles.navLink} onClick={handleClick}>
+                        TARIFS & PRÉSATIONS
+                    </a>
                 </li>
                 <li className={styles.navItem}>
-                    <a className={styles.navLink}>CONTACT</a>
+                    <a className={styles.navLink} onClick={handleClick}>
+                        CONTACT
+                    </a>
+                </li>
+                <li className={styles.navItem}>
+                    <Link href="/">
+                        <a className={styles.navLink} onClick={handleClick}>
+                            ACCUEIL
+                        </a>
+                    </Link>
                 </li>
             </ul>
-            <div className={styles.hamburger}>
+            <div className={hamburgerClassName} onClick={handleClick}>
                 <span className={styles.bar} />
                 <span className={styles.bar} />
                 <span className={styles.bar} />
