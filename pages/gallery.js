c9939b01cfd22d9e890782d93b1559f5ec3d5319
Emilien <emilien.billy@gmail.com>
2022-06-22 17:25:25 +0200

    Fetching photos in gallery.js page from photoGallery content model from Contentful

--- pages/gallery.js
+++ pages/gallery.js
@@ -1,7 +1,32 @@
-const Gallery = () => {
+import { createClient } from "contentful";
+import styles from "../styles/Gallery.module.css";
+import Image from "next/image";
+
+export async function getStaticProps() {
+    const client = createClient({
+        space: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+    });
+
+    const res = await client.getEntries({ content_type: "photoGallery" });
+
+    return {
+        props: {
+            photos: res.items,
+        },
+    };
+}
+
+const Gallery = ({ photos }) => {
+    console.log(photos);
     return (
-        <div>
+        <div className={styles.container}>
             <h1>Gallerie</h1>
+            <div className={styles.grid}>
+                {photos.map((photo) => (
+                    <Image key={photo.sys.id} src={"https:" + photo.fields.image.fields.file.url} width={350} height={200} objectFit={"cover"} alt="" />
+                ))}
+            </div>
         </div>
     );
 };
